66821c8a926d0db0a2245ebad4d7d2fc380db946
kaio-fabiano <kaio.fabiano@outlook.com>
2023-10-19 11:11:47 -0300

    Revert "refactor: change dto`s tipes"
    This reverts commit f9198aeee5cb369bd21347cf2db5e0c6a2c77ebb.

--- userapi/src/main/java/com/shop/userapi/user/dto/UpdateUserDTO.java
+++ userapi/src/main/java/com/shop/userapi/user/dto/UpdateUserDTO.java
@@ -1,6 +1,19 @@
 package com.shop.userapi.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
-public record UpdateUserDTO(String name, String email, String password, String phone) {}
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserDTO {
+  private String name;
+  private String email;
+  private String password;
+  private String phone;
+}
