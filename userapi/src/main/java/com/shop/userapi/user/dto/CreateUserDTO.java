b1e8cf193d04bc563f7886ecb15ed3255ab815c8
kaio-fabiano <kaio.fabiano@outlook.com>
2023-10-19 11:12:59 -0300

    refactor: change dto`s types

--- userapi/src/main/java/com/shop/userapi/user/dto/CreateUserDTO.java
+++ userapi/src/main/java/com/shop/userapi/user/dto/CreateUserDTO.java
@@ -3,28 +3,13 @@ package com.shop.userapi.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import java.io.Serializable;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.validator.constraints.br.CPF;
 
-@Getter
-@Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class CreateUserDTO implements Serializable {
-  @CPF @NotBlank private String CPF;
-
-  @NotBlank private String name;
-
-  @Email private String email;
-
-  @NotBlank private String password;
-
-  @Pattern(regexp = "^\\([1-9]{2}\\) [9]{0,1}[6-9]{1}[0-9]{3}\\-[0-9]{4}$")
-  private String phone;
-}
+public record CreateUserDTO(
+    @CPF @NotBlank String CPF,
+    @NotBlank String name,
+    @Email String email,
+    @NotBlank String password,
+    @Pattern(regexp = "^\\([1-9]{2}\\) [9]{0,1}[6-9]{1}[0-9]{3}\\-[0-9]{4}$") String phone) {}
--- userapi/src/main/java/com/shop/userapi/user/dto/UpdateUserDTO.java
+++ userapi/src/main/java/com/shop/userapi/user/dto/UpdateUserDTO.java
@@ -1,19 +1,6 @@
 package com.shop.userapi.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class UpdateUserDTO {
-  private String name;
-  private String email;
-  private String password;
-  private String phone;
-}
+public record UpdateUserDTO(String name, String email, String password, String phone) {}
