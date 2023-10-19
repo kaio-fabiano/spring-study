f9198aeee5cb369bd21347cf2db5e0c6a2c77ebb
kaio-fabiano <kaio.fabiano@outlook.com>
2023-10-19 11:11:03 -0300

    refactor: change dto`s tipes

--- userapi/src/main/java/com/shop/userapi/user/dto/UserDTO.java
+++ userapi/src/main/java/com/shop/userapi/user/dto/UserDTO.java
@@ -2,18 +2,13 @@ package com.shop.userapi.user.dto;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class UserDTO {
-  private UUID id;
-  private String CPF;
-  private String name;
-  private String email;
-  private String password;
-  private String phone;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
-}
+public record UserDTO(
+    UUID id,
+    String CPF,
+    String name,
+    String email,
+    String password,
+    String phone,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt) {}
